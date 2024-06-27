fe0890d78bca130462602a940aeeb519e2174df0
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-06-27 14:58:37 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,15 @@
+console.log(2 > 1);
+console.log(2 < 1);
+
+console.log("2" > 1);
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log("2" == 2);
+console.log("2" === 2);
+// three equals lagany sy wo btae ga k inka datatype different hay
+// 2 equal sy equality btae ga
